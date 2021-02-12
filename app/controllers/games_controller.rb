a725271820fe84cd8cefd54b41ffab69b30d81af
jeremy wilson <jeremyw9187@gmail.com>
2021-02-11 19:36:38 -0500

    making progress with new page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,19 @@
 class GamesController < ApplicationController
 
   def new
+    make_letters_array
   end
 
   def score
+  end
+
+  private
+
+  def make_letters_array
+    aplphabet_array = ('a'..'z').to_a
+    @letters_array = []
+    10.times do
+      @letters_array << aplphabet_array.sample
+    end
   end
 end
