ae210adce5da4ed61bdb44109361cb581ccceb0e
jeremy wilson <jeremyw9187@gmail.com>
2021-11-09 08:54:24 -0500

    fixing syntax

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,21 +1,35 @@
+require "open-uri"
+
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
 
   def new
-    make_letters_array
+    make_letters
   end
 
   def score
-    @letters_array = params[:letters_array]
-    @answer = params[:answer]
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
   end
 
   private
 
-  def make_letters_array
-    alphabet_array = ('A'..'Z').to_a
-    @letters_array = []
-    10.times do
-      @letters_array << alphabet_array.sample
-    end
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
   end
+
+  def make_letters
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+
 end
